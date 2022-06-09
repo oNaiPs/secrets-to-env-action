4368bdf2c540cdd48df0f1b3a287b34c513f7726
Jose Luis Pereira <onaips@gmail.com>
2022-06-09 22:13:51 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+// import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const secrets: Object = core.getInput('secrets')
+    // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.info(`Got Secrets!`)
+    core.info(JSON.stringify(secrets))
 
-    core.setOutput('time', new Date().toTimeString())
+    // core.debug(new Date().toTimeString())
+    // await wait(parseInt(ms, 10))
+    // core.debug(new Date().toTimeString())
+
+    // core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
